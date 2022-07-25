48bc7b57d867a2426e26b8b1a208cce8a8c524b5
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-07-25 13:23:23 -0500

    wrote movie routes

--- controllers/api/Movie-routes.js
+++ controllers/api/Movie-routes.js
@@ -1,10 +1,23 @@
-const router = require('express').Router();
+const { application } = require('express');
+var express = require('express').Router();
+var router  = express.Router();
+var movie = require('../../models/movie');
+application.use("../../models/movie", movie);
+
+//get a movie from the list
+router.get("https://imdb-api.com/en/API/SearchMovie/k_12345678/:id", function (req, res) {
+    res.send('List of movies.');
+});
+
+//post movie to watchlist
+router.post('/movie', function (req, res) {
+    res.send('Posted to movies list.')
+});
+
+//delete movie from watchlist
+router.delete('/movie', function (req, res) {
+    res.send('Deleted from watch list.')
+});
 
 
-router.get('/', (req, res) => {
-    res.json({
-        message: 'message'
-    })
-})
-
-module.exports = router
+module.export = router;
