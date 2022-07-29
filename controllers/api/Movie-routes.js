67de46dbe518957665e2cc09dbf41d7963426aba
NangTuong <nguyen.brad@outlook.com>
2022-07-28 21:41:18 -0400

    updated movie routes to add and delete from watchlist

--- controllers/api/Movie-routes.js
+++ controllers/api/Movie-routes.js
@@ -1,24 +1,69 @@
 var router = require('express').Router();
-const { Movie } = require('../../models/Movie');
-
-//get a movie from the list
-router.get("https://imdb-api.com/en/API/SearchMovie/k_12345678/:id", function (req, res) {
-    res.send('List of movies.');
-});
+const { Movie, Watchlist } = require('../../models');
 
 //post movie to watchlist
 router.post('/', function (req, res) {
-    data = req.body
-    console.log(data)
-    res.json(data)
+    const data = req.body; 
+    const imdbId = data.imdb_id;
+    const userId = req.session.user_id;
+    
+    Movie.findOne({
+        where: {
+            imdb_id: imdbId
+        }
+    }).then(dbMovieData => {
+        if(dbMovieData) {
+            Watchlist.findOne({
+                where:  {
+                user_id: userId, 
+                movie_id: dbMovieData.id
+                }
+            }).then(dbWatchlistData => {
+                if (dbWatchlistData) {
+                    res.status(404).json({
+                        message: 'You already have this movie added to your watchlist.',
+                        type: 'is-danger'
+                    })
+                }
+                else {
+                    Watchlist.create(
+                        {
+                            user_id: userId, 
+                            movie_id: dbMovieData.id
+                        }
+                    ).then(dbMovieData => {
+                        res.status(200).json({
+                            message: dbMovieData.title + ' has been added to your Watchlist!', 
+                            type: 'is-success'
+                        })
+                    })
+                }
+            })
+        }
+    })
 });
 
 //delete movie from watchlist
-router.delete('/movie', function (req, res) {
-    res.send('Deleted from watch list.')
+router.delete('/:id', function (req, res) {
+    Watchlist.delete({
+        where: {
+            movie_id: req.params.id, 
+            user_id: req.session.user_id
+        }
+    }).then(dbWatchlistData => {
+        Movie.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(dbMovieData => {
+            res.status(200).json({
+                message: dbMovieData.title + ' has been deleted from your watchlist!', 
+                type: 'is-success'
+            })
+        })
+    })
 });
-
-/*TESTTEST*/
 
 
 module.exports = router;
+
