415a215a990ffd6722ae62a8c28128e751c2906b
NangTuong <nguyen.brad@outlook.com>
2022-07-30 16:27:13 -0400

    added classes to change layout and button colors. added helpers.js in utils to display number of watchlists a movie appears on

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -76,10 +76,10 @@ const displayResults = results => {
                     <p id="${movie.id}-title" class="title is-size-6 movie-title">
                     ${movie.title}
                     </p>
-                    <button value="${movie.id}" class="button is-small movie-button" onclick="viewMovie(event)">
+                    <button value="${movie.id}" class="button is-small movie-button is-info is-outlined is-rounded" onclick="viewMovie(event)">
                     View Movie
                     </button>
-                    <button value="${movie.id}" class="button is-small movie-button" onclick="addToWatchlist(event)">
+                    <button value="${movie.id}" class="button is-small movie-button is-success is-outlined is-rounded" onclick="addToWatchlist(event)">
                     Add to Watchlist
                     </button>
                 </div>
